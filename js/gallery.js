1d9477817d57c7aeb271b58bef63633f66cbb2a5
Tetiana-Bobrova <74472422+Tetiana-Bobrova@users.noreply.github.com>
2021-08-19 16:38:29 +0200

    Open Modal

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,11 @@
 import galleryItems from "../app.js";
-console.log(galleryItems);
+
 const galleryItemList = document.querySelector('.js-gallery');
+const modalEl = document.querySelector('.js-lightbox');
+const modalCloseBtnEl = document.querySelector('[data-action="close-lightbox"]');
+const imageOrigin = document.querySelector('.lightbox__image');
+const backdropEl = document.querySelector('.lightbox__overlay');
+
 const galleryListMarkup = createGalleryItemsMarkup(galleryItems);
 galleryItemList.insertAdjacentHTML('afterbegin', galleryListMarkup);
 
@@ -26,9 +31,28 @@ function createGalleryItemsMarkup(galleryItems) {
 
 
 galleryItemList.addEventListener('click', onOpenModal);
+modalCloseBtnEl.addEventListener('click', onCloseModal);
+backdropEl.addEventListener('click', onCloseModal);
+window.addEventListener('keyup', onCloseModal);
 
 function onOpenModal(event) {
+    event.preventDefault();
     console.log(event.target.nodeName);
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    modalEl.classList.add('is-open');
+    imageOrigin.setAttribute('src', event.target.dataset.source);
+    imageOrigin.setAttribute('alt', event.target.alt);
+    
     console.log("Открытие модального окна");
 
+}
+
+function onCloseModal(event) {
+  if (event.target===event.currentTarget || event.key === "Escape"){
+    modalEl.classList.remove('is-open');
+    imageOrigin.removeAttribute('src');
+    imageOrigin.removeAttribute('alt');
+  }
 }
